4d7062c02ff2a06531a1e3f56b861ef2a5f4d6cc
mehmetCt <mehmet@cybertekschool.com>
2025-02-21 16:25:53 -0500

    B37 Init

--- src/test/java/cydeo/utilities/Driver.java
+++ src/test/java/cydeo/utilities/Driver.java
@@ -48,7 +48,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "100.27.0.96";
+                        String gridAddress = "44.202.6.42";
                         URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
                         ChromeOptions chromeOptions = new ChromeOptions();
                         chromeOptions.addArguments("--start-maximized");
